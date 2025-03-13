f0bd727947a59920703c85346da5037942df75b1
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-13 01:12:45 -0500

    Update ApplicationForm Enum Syntax
    The previous enum syntax caused problems when tryingto run or test the
    application. This updated syntax should work correctly now.

--- template/{{app_name}}/engines/flex/app/models/flex/application_form.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/application_form.rb
@@ -2,6 +2,6 @@ module Flex
   class ApplicationForm < ApplicationRecord
     self.abstract_class = true
 
-    enum status: { in_progress: 0, in_review: 1, processed: 2 }
+    enum :status, in_progress: 0, submitted: 1
   end
 end
