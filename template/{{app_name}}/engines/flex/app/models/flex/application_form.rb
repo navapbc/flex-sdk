8226a767c36c31cc922a4626e8f793a561c6eb3b
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-13 04:14:59 -0500

    Added status attribute to application form
    The model wants an attribute to be clearly defined with its type
    before it can be used as an enum, according to the error. Adding this
    attribute fixed the error.

--- template/{{app_name}}/engines/flex/app/models/flex/application_form.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/application_form.rb
@@ -1,7 +1,8 @@
 module Flex
   class ApplicationForm < ApplicationRecord
     self.abstract_class = true
-
+    
+    attribute :status, :integer, default: 0
     enum :status, in_progress: 0, submitted: 1
   end
 end
