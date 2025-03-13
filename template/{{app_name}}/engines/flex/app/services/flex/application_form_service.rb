d366abc23db9051b06825262068c0dfdaaa0692e
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-13 04:26:44 -0500

    Adding service layer for application form
    I added this ApplicationFormService for the purpose of separating the
    business logic from the data logic. While the models and repositories
    (if the repositories are kept) will be responsible for any data logic,
    the service will be responsible for any specific business logic, such
    as  the current requirement of checking if a form is already submitted
    before allowing any updates to it.
    
    Since this service is where most of the business logic is focused, I
    test-drove its creation. I started this in a more "traditional" Ruby on
    Rails style (from my understanding), however I also learned about a more
    BDD style after this was finished. I may consider switching to that
    style in the future.

---
+++ template/{{app_name}}/engines/flex/app/services/flex/application_form_service.rb
@@ -0,0 +1,33 @@
+module Flex
+  class ApplicationFormService
+    def initialize(repository: ApplicationFormRepository.new)
+      @repository = repository
+    end
+
+    def create_form(params)
+      @repository.create(params)
+    end
+
+    def update_form(id, params)
+      raise ArgumentError, "Invalid id (#{id.inspect})" unless id.present?
+      raise ArgumentError, "Invalid params (#{params.inspect})" unless params.present?
+
+      if is_status_submitted?(id)
+        raise "Application form for id #{id} is already submitted and can no longer be modified"
+      else
+        @repository.update(id, params)
+      end
+    end
+
+    def submit_form(id, params: {})
+      update_form(id, params.merge(status: ApplicationForm.statuses[:submitted]))
+    end
+
+    private
+
+    def is_status_submitted?(id)
+      status = @repository.find_fields(id, ['status'])
+      status == ApplicationForm.statuses[:submitted]
+    end
+  end
+end
