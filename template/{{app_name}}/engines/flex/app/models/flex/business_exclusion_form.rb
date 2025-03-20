25bcfa920e55c12c41dbe681fea4a56d4d8941bb
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-19 21:47:13 -0500

    Removing test files from the application directory
    These were previously moved here while attempting to get testing to
    work. The business_exclusion_form is now in the spec directory as
    test_business_exclusion_form.rb. The repositories are no longer needed.
    The migration has been appropriatedly copied into the spec directory.

--- template/{{app_name}}/engines/flex/app/models/flex/business_exclusion_form.rb
+++
@@ -1,6 +0,0 @@
-module Flex
-  class BusinessExclusionForm < ApplicationForm
-    attribute :business_name, :string
-    attribute :business_type, :string
-  end
-end
--- template/{{app_name}}/engines/flex/app/repositories/flex/business_exclusion_form_repository.rb
+++
@@ -1,9 +0,0 @@
-module Flex
-  class BusinessExclusionFormRepository < BaseRepository
-    def initialize
-      super(BusinessExclusionForm)
-    end
-
-    # Additional specific methods for BusinessExclusionForm can be added here
-  end
-end
