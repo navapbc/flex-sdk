d914d86a7eed44a7e27aa304bf0eef1b11a67e11
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-20 11:09:28 -0500

    Removing factory_bot_rails from flex
    At this time it's not needed, so I'm going to remove it from this
    branch. It can easily be added back in later, if needed.

--- template/{{app_name}}/engines/flex/spec/dummy/config/application.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/config/application.rb
@@ -18,8 +18,6 @@ module Dummy
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    config.factory_bot.definition_file_paths = [ "spec/factories" ]
-
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
--- template/{{app_name}}/engines/flex/spec/factories/flex/test_exclusion_form_factory.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :flex_test_exclusion_form do
-    business_name { "Business 1" }
-    business_type  { "Restaurant" }
-  end
-end
