4d08520501f995738e636395f4677b827ce657a5
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-27 10:29:13 -0500

    Deleted case model migration
    Since Case is an abstract model, we don't need a migration for it.

--- template/{{app_name}}/engines/flex/db/migrate/20250327054032_create_flex_cases.rb
+++
@@ -1,9 +0,0 @@
-class CreateFlexCases < ActiveRecord::Migration[8.0]
-  def change
-    create_table :flex_cases do |t|
-      t.integer :status, default: 0, null: false
-
-      t.timestamps
-    end
-  end
-end
