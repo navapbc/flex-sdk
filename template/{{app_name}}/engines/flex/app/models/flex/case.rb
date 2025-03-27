8abd9167f1aafc647de3f441436210f39640d391
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-03-27 11:13:10 -0500

    Fixed case model to allow internal status updates
    Using the `update` method throws an error related to trying to update
    a protected field (status). Using `self[:status]` allows us to update
    the status without that problem.

--- template/{{app_name}}/engines/flex/app/models/flex/case.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/case.rb
@@ -7,11 +7,13 @@ module Flex
     enum :status, open: 0, closed: 1
 
     def close
-      update(status: :closed)
+      self[:status] = :closed
+      save
     end
 
     def reopen
-      update(status: :open)
+      self[:status] = :open
+      save
     end
   end
 end
