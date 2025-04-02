7adbf049c084693c15ae63fb5517ca3b610a1bff
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-02 00:04:09 -0500

    Adding happy path acceptance test (WIP)
    Noting that this test is currently in-progress and will be changed
    significantly in the near future. Right now this file is better serving
    as a "sanity test" while I'm implementing features and overcoming
    Rails difficulties.

--- template/{{app_name}}/engines/flex/spec/tbd/passport_application_case_acceptance_spec.rb
+++ template/{{app_name}}/engines/flex/spec/tbd/passport_application_case_acceptance_spec.rb
@@ -1,9 +1,40 @@
 require 'rails_helper'
 require_relative '../dummy/app/models/flex/business_processes/passport_application_business_process'
+require_relative '../../app/models/concerns/step'
 
 module Flex
   RSpec.describe PassportApplicationForm, type: :model do
     let(:test_form) { described_class.new }
+
+    describe 'when starting a new form' do
+      it 'happy path' do
+        test_form.first_name = 'John'
+        test_form.last_name = 'Doe'
+        test_form.date_of_birth = Date.new(1990, 1, 1)
+        test_form.save!
+
+        expect(test_form.valid?).to be true
+
+        kase = test_form.passport_case
+        expect(kase).not_to be_nil
+
+        business_process = kase.business_processes.first
+        expect(business_process).not_to be_nil
+        expect(business_process.name).to eq('Passport Application Process')
+
+        business_process.execute(kase)
+        expect(business_process.current_step).to eq('collect_user_info')
+
+        test_form.submit_application
+        expect(test_form.status).to eq('submitted')
+
+        business_process.execute(kase)
+        expect(business_process.current_step).to eq('collect_user_info')
+      end
+    end
+
+
+
 
     describe 'on create' do
       it 'creates a new passport application case' do
