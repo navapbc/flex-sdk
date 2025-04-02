e1720d88c96871b097e35e6ae138a01af38d753e
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-01 20:44:26 -0500

    Add iterative execute method to business process
    Updated the execute method to iteratively go through the defined
    steps and call execute on each of the steps. If all of the steps return
    true, it will also mark the business process as completed.

--- template/{{app_name}}/engines/flex/app/models/flex/business_process.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/business_process.rb
@@ -8,7 +8,7 @@ module Flex
     attr_accessor :name, :description, :steps, :current_step
 
     attribute :status, :integer, default: 0
-    enum status: { pending: 0, in_progress: 1, completed: 2, failed: 3 } # Just temporary statuses
+    enum status: { pending: 0, in_progress: 1, completed: 2 } # Just temporary statuses
 
     validates :name, presence: true
     
@@ -22,13 +22,42 @@ module Flex
       self.current_step = first_step_key
       self.save!
 
-      step.execute(self.kase)
+      execute_steps(steps)
+    end
+
+    def completed?
+      status == "completed"
     end
 
     protected
 
-    def get_steps()
+    def get_steps
       raise NotImplementedError, "Subclasses must implement the `get_steps` method"
+    end
+
+    private
+
+    def execute_steps(steps)
+      while !completed?
+        step = steps[current_step]
+        result = step.execute(kase)
+        if result
+          increment_current_step(steps)
+        end
+        break unless result
+      end
+    end
+
+    def increment_current_step(steps)
+      keys = steps.keys
+      current_index = keys.index(current_step)
+      if current_index && current_index + 1 < keys.size
+        self.current_step = keys[current_index + 1]
+        self.save!
+      elsif current_step == keys.last
+        self.status = "completed"
+        self.save!
+      end
     end
   end
 end
