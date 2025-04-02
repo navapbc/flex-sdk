95d520aa02c8de12c13cee6908e73adbd197db09
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-02 00:02:39 -0500

    Overriding submit_application for passport form
    This is likely temporary, however I've modified the logic of the
    PassportApplicationForm to better test out the Case module logic. The
    submit_application method now checks to see if all necessary fields
    are present before appropriately submitting the application.

--- template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/forms/passport_application_form.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/forms/passport_application_form.rb
@@ -6,7 +6,17 @@ module Flex
 
     has_one :passport_case, class_name: 'Flex::PassportCase', foreign_key: :passport_application_form_id, dependent: :destroy
 
+    before_update :prevent_submition_if_missing_fields, if: :is_submitting?
     after_create :create_passport_case
+
+    def submit_application
+      if has_all_necessary_fields?
+        super
+      else
+        errors.add(:base, "Missing necessary fields for passport application")
+        false
+      end
+    end
 
     def has_all_necessary_fields?
       first_name.present? && last_name.present? && date_of_birth.present?
@@ -16,6 +26,13 @@ module Flex
 
     def create_passport_case
       passport_case = PassportCase.create(passport_application_form: self)
+    end
+
+    def prevent_submition_if_missing_fields
+      if !has_all_necessary_fields?
+        errors.add(:base, "Missing necessary fields for passport application")
+        throw :abort
+      end
     end
   end
 end
