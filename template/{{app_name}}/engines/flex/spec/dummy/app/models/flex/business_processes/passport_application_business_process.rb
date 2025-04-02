05a347380ad156e51f7d58d8a2eb01e5cbe0215e
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-02 00:01:27 -0500

    Update step keys and required files
    Updated the keys to make more sense. Also, I was getting errors
    when running tests due to the now-required files not being included
    previously. This was fixed upon requiring them.

--- template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/business_processes/passport_application_business_process.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/business_processes/passport_application_business_process.rb
@@ -1,13 +1,17 @@
+require_relative '../steps/user_tasks/collect_user_information_user_task'
+require_relative '../steps/user_tasks/take_passport_photo_user_task'
+require_relative '../steps/system_processes/verify_identity_system_process'
 module Flex
   class PassportApplicationBusinessProcess < BusinessProcess
+    # belongs_to :case, class_name: 'Flex::Case', foreign_key: 'case_id'
     
     protected
     
     def get_steps
       {
-        "step_1": CollectUserInformationUserTask.new,
-        "step_2": TakePassportPhotoUserTask.new,
-        "step_3": VerifyIdentitySystemProcess.new
+        "collect_user_info" => CollectUserInformationUserTask.new,
+        "take_passport_photo" => PassportPhotoUserTask.new,
+        "verify_system_identity" => VerifyIdentitySystemProcess.new
       }
     end
   end
