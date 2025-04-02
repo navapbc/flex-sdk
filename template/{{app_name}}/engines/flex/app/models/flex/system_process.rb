3de61d04f5586711f0b8affd14bb5e4fc53a244d
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-01 20:43:57 -0500

    Renamed system_task to system_process

--- template/{{app_name}}/engines/flex/app/models/flex/system_process.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/system_process.rb
@@ -1,21 +1,18 @@
 module Flex
-  class SystemTask
+  class SystemProcess
     include Step
     include ActiveModel::Model
     include ActiveModel::Validations
     include ActiveModel::Conversion
     extend ActiveModel::Naming
+    self.abstract_class = true
 
     attr_accessor :name
 
     validates :name, presence: true
-
-    def initialize(callback)
-      @callback = callback
-    end
     
     def execute(kase)
-      @callback(kase)
+      raise NoMethodError, "Children must implement the `execute` method"
     end
 
     def persisted?
