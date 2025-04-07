a2e37d0055d7eaf625dd065d469afb3984820891
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-06 23:25:24 -0500

    Fix business_process field definition
    I had mistakenly swapped the "readonly" and "attribute" keywords

--- template/{{app_name}}/engines/flex/app/models/flex/case.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/case.rb
@@ -6,8 +6,10 @@ module Flex
     protected attr_writer :status, :integer
     enum :status, open: 0, closed: 1
 
-    attribute readonly :business_process
+    readonly attribute :business_process
     attribute :business_process_current_step, :string
+
+    # tasks: Task[] Maybe?
 
     def close
       self[:status] = :closed
