d8d5a6cef953d7a063a53f2f279c116ed2929382
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-08 12:57:44 -0500

    Add and update step methods
    The steps set the next step in the process as well as update
    any relevant boolean flag on the class.

--- template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/passport_case.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/passport_case.rb
@@ -2,16 +2,46 @@ require_relative "../../factories/business_process_simple_factory"
 module Flex
   class PassportCase < Case
     readonly attribute :passport_id, :string, default: SecureRandom.uuid # always defaults to a new UUID
+
     attribute :business_process_current_step, :string, default: "collect application info"
+    private def business_process_current_step=(value)
+      self[:business_process_current_step] = value
+    end
+
+    attribute :is_application_info_collected, :boolean, default: false
+    private def is_application_info_collected=(value)
+      self[:is_application_info_collected] = value
+    end
+    
+    attribute :is_passport_photo_reviewed, :boolean, default: false
+    private def is_passport_photo_reviewed=(value)
+      self[:is_passport_photo_reviewed] = value
+    end
+    
+    attribute :is_information_verified, :boolean, default: false
+    private def is_information_verified=(value)
+      self[:is_information_verified] = value
+    end
+
+    validates :is_application_info_collected, :is_passport_photo_reviewed, :is_information_verified, inclusion: { in: [true, false] }
 
     readonly @business_process = BusinessProcessSimpleFactory::create_passport_application_business_process
+
+    def mark_application_info_collected
+      self[:is_application_info_collected] = true
+      self[:business_process_current_step] = "verify identity"
+      save!
+    end
   
     def verify_identity
-      update!(business_process_current_step: "review passport photo")
+      self[:is_information_verified] = true
+      self[:business_process_current_step] = "review passport photo"
+      save!    
     end
 
     def approve
-      update!(business_process_current_step: "end")
+      self[:is_passport_photo_reviewed] = true
+      self[:business_process_current_step] = "end"
       close
     end
 
