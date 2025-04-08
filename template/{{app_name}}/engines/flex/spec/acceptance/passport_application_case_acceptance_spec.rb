c2b8ab7ea30bbf4e097e682039a815f54e38cb06
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-08 17:09:26 -0500

    Adding relative reference to fix CI test error

--- template/{{app_name}}/engines/flex/spec/acceptance/passport_application_case_acceptance_spec.rb
+++ template/{{app_name}}/engines/flex/spec/acceptance/passport_application_case_acceptance_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require_relative '../dummy/app/business_processes/passport_application_business_process_manager'
 
 module Flex
   RSpec.describe PassportApplicationForm, type: :model do
