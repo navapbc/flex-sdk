37ea1b7a14e0aa948060007200dcbeee2c5239de
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-08 17:13:14 -0500

    Adding business_processes to eager load config
    This is to test and see if this solves a CI testing issue that
    isn't replicated locally.

--- template/{{app_name}}/engines/flex/spec/dummy/config/application.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/config/application.rb
@@ -24,6 +24,6 @@ module Dummy
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.eager_load_paths << Rails.root.join("business_processes")
   end
 end
