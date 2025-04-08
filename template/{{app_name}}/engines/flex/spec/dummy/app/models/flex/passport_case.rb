875acc72368477c19a5b578c586db18bd486966c
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-08 15:50:01 -0500

    Remove unnecessary class properties

--- template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/passport_case.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/app/models/flex/passport_case.rb
@@ -8,39 +8,19 @@ module Flex
       self[:business_process_current_step] = value
     end
 
-    attribute :is_application_info_collected, :boolean, default: false
-    private def is_application_info_collected=(value)
-      self[:is_application_info_collected] = value
-    end
-
-    attribute :is_passport_photo_reviewed, :boolean, default: false
-    private def is_passport_photo_reviewed=(value)
-      self[:is_passport_photo_reviewed] = value
-    end
-
-    attribute :is_information_verified, :boolean, default: false
-    private def is_information_verified=(value)
-      self[:is_information_verified] = value
-    end
-
-    validates :is_application_info_collected, :is_passport_photo_reviewed, :is_information_verified, inclusion: { in: [ true, false ] }
-
     readonly @business_process = BusinessProcessSimpleFactory.create_passport_application_business_process
 
     def mark_application_info_collected
-      self[:is_application_info_collected] = true
       self[:business_process_current_step] = "verify identity"
       save!
     end
 
     def verify_identity
-      self[:is_information_verified] = true
       self[:business_process_current_step] = "review passport photo"
       save!
     end
 
     def approve
-      self[:is_passport_photo_reviewed] = true
       self[:business_process_current_step] = "end"
       close
     end
