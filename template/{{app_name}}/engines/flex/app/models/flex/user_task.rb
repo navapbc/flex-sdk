798f1b35520633940e2571e013efe710ad81d409
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-09 14:27:08 -0500

    Adding user task class

---
+++ template/{{app_name}}/engines/flex/app/models/flex/user_task.rb
@@ -0,0 +1,20 @@
+module Flex
+  class UserTask
+    include ActiveModel::Model
+    include ActiveModel::Validations
+    include Step
+
+    attr_accessor :name
+
+    validates :name, presence: true
+
+    def initialize(task_management_service)
+      @task_management_service = task_management_service
+      # @task_management_service.create_task
+    end
+
+    def execute(kase)
+      @task_management_service.create_task(name, "eventually will contain details about a case instead of static string")
+    end
+  end
+end
