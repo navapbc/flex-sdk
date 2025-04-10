24441b2bc2e137c4ce8a9e7be29a0e0010d8f6f2
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-10 11:47:37 -0500

    Remove execute method from class_methods
    The execute method will need to be an instance method, not a class
    method.

--- template/{{app_name}}/engines/flex/app/models/concerns/flex/step.rb
+++ template/{{app_name}}/engines/flex/app/models/concerns/flex/step.rb
@@ -2,10 +2,8 @@ module Flex
   module Step
     extend ActiveSupport::Concern
 
-    class_methods do
-      def execute(kase)
-        raise NoMethodError, "#{self.class} must implement execute method"
-      end
+    def execute(kase)
+      raise NoMethodError, "#{self.class} must implement execute method"
     end
   end
 end
