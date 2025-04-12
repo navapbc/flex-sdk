c6fed13d61389aa67827fad9c5d7fc076e4ad26c
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-11 19:30:00 -0500

    Update transitions, steps, and event listening
    This matches the new structure after conversations between
    myself and Loren.

--- template/{{app_name}}/engines/flex/app/models/flex/business_process.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/business_process.rb
@@ -4,13 +4,15 @@ module Flex
 
     attr_accessor :name, :description, :steps, :start, :transitions
 
-    def initialize(name:, description: "", steps: {}, start: "", transitions: {})
+    def initialize(name:, type:, description: "", steps: {}, start: "", transitions: {})
+      @subscriptions = {}
       @name = name
+      @type = type # recognizing this is a code smell that we will want to address in the future
       @description = description
-      @steps = steps
-      @start = start
-      @transitions = transitions
-      @event_listeners = {}
+      define_start(start)
+      define_steps(steps)
+      define_transitions(transitions)
+      start_listening_for_events
     end
 
     def execute(kase)
@@ -26,24 +28,43 @@ module Flex
     end
 
     def define_transitions(transitions)
+      if(@subscriptions.any?)
+        stop_listening_all_events
+      end
+
       @transitions = transitions
+      start_listening_for_events
     end
 
-    def add_event_listener(event_key, callback)
-      raise "Event listener for #{event_key} already exists" if @event_listeners.key?(event_key)
+    private
 
-      @event_listeners[event_key] = EventsManager.subscribe(event_key, callback)
+    def handle_event(event)
+      kase = @type.find(event[:payload][:case_id])
+      current_step = kase.business_process_current_step
+      next_step = @transitions[current_step][event[:name]]
+      kase.business_process_current_step = next_step
+      kase.save!
+      @steps[next_step].execute(kase)
     end
 
-    def remove_event_listener(event_key)
-      raise "No event listener found for #{event_key}" unless @event_listeners.key?(event_key)
-
-      EventsManager.unsubscribe(@event_listeners[event_key])
-      @event_listeners.delete(event_key)
+    def get_event_names_from_transitions
+      @transitions.values.flat_map(&:keys).uniq
     end
 
-    def get_events_being_listened_to
-      @event_listeners.keys
+    def start_listening_for_events
+      get_event_names_from_transitions.each do |event_name|
+        new_subscription = EventsManager.subscribe(event_name, -> (event) {
+          handle_event(event)
+        })
+        @subscriptions[event_name] = new_subscription
+      end
+    end
+
+    def stop_listening_all_events
+      @subscriptions.each do |event_name, subscription|
+        EventsManager.unsubscribe(subscription)
+      end
+      @subscriptions.clear
     end
   end
 end
