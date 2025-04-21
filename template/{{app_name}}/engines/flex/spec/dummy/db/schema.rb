b98b50e65e2c06cae4aff1e2215540809feff264
Loren Yu <loren@navapbc.com>
2025-04-21 16:00:57 -0700

    Update schema.rb

--- template/{{app_name}}/engines/flex/spec/dummy/db/schema.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/db/schema.rb
@@ -10,39 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2024_12_02_180940) do
-  create_table :flex_test_application_forms do |t|
-    t.integer :status, default: 0
-    t.string :test_string
-
-    t.timestamps
+ActiveRecord::Schema[8.0].define(version: 2025_03_27_160205) do
+  create_table "flex_passport_application_forms", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.date "date_of_birth"
+    t.integer "status", default: 0
+    t.integer "case_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["case_id"], name: "index_flex_passport_application_forms_on_case_id", unique: true
   end
 
-  create_table :flex_test_cases do |t|
-    t.integer :status, default: 0, null: false
-    t.string :business_process_current_step
-
-    t.timestamps
+  create_table "flex_passport_cases", force: :cascade do |t|
+    t.integer "status", default: 0, null: false
+    t.string "passport_id", limit: 36, null: false
+    t.string "business_process_current_step"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table :flex_passport_application_forms, force: :cascade do |t|
-    t.string :first_name
-    t.string :last_name
-    t.date :date_of_birth
-    t.integer :status, default: 0
-    t.integer :case_id
-
-    t.timestamps
-  end
-
-  create_table :flex_passport_cases do |t|
-    t.integer :status, default: 0, null: false
-    t.string :passport_id, null: false, limit: 36 # Is a UUID, which is always exactly 36 characters
-    t.string :business_process_current_step
-
-    t.timestamps
-  end
-
-  add_index :flex_passport_application_forms, :case_id, unique: true
-  add_foreign_key :flex_passport_application_forms, :flex_passport_cases, column: :case_id, primary_key: :id, on_delete: :cascade
+  add_foreign_key "flex_passport_application_forms", "flex_passport_cases", column: "case_id", on_delete: :cascade
 end
