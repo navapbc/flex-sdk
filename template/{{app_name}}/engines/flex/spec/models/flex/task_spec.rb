f277dff7922611ffe0babd2a480f10a4c16f53f5
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-04-30 12:24:26 -0500

    Added task model tests to cover more methods
    Specifically because of the polymorphic associations, these are
    a little more useful.

--- template/{{app_name}}/engines/flex/spec/models/flex/task_spec.rb
+++ template/{{app_name}}/engines/flex/spec/models/flex/task_spec.rb
@@ -1,15 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Flex::Task, type: :model do
-  describe 'status attribute' do
-    let(:task) { described_class.new }
+  let(:task) { described_class.new }
 
+  describe 'status attribute' do
     it 'does not allow status to be modified directly' do
       expect { task.status = :completed }.to raise_error(NoMethodError)
     end
+  end
 
-    it 'allows status to be modified via mark_completed method' do
+  describe '#assign' do
+    let(:user) { User.create! }
+    
+    it 'assigns the task to the given user' do
+      task.assign(user)
+      
+      expect(task.assignee).to eq(user)
+    end
+  end
+  
+  describe '#unassign' do
+    let(:user) { User.create! }
+
+    before do
+      task.assignee = user
+      task.save!
+    end
+
+    it 'removes the assignee from the task' do
+      task.unassign
+
+      expect(task.assignee).to be_nil
+    end
+  end
+
+  describe '#mark_completed' do
+    it 'marks the task as completed' do
       task.mark_completed
+
       expect(task.status).to eq('completed')
     end
   end
