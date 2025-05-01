ef0ecd0f348a2fcea8a8627c9e3469885d1c098d
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-01 11:12:35 -0500

    Remove unnecessary default for primary key

--- template/{{app_name}}/engines/flex/spec/dummy/db/migrate/20250430200016_add_users.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/db/migrate/20250430200016_add_users.rb
@@ -1,6 +1,6 @@
 class AddUsers < ActiveRecord::Migration[8.0]
   def change
-    create_table :users, id: :uuid, default: -> { "gen_random_uuid()" } do |t|
+    create_table :users, id: :uuid do |t|
       t.string :name
 
       t.timestamps
