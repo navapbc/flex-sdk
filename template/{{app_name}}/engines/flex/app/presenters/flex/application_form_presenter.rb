42387df40b39775eff08294365e6a2b7476fd8eb
Loren Yu <loren@navapbc.com>
2025-05-01 16:39:09 -0700

    Add reusable show view

---
+++ template/{{app_name}}/engines/flex/app/presenters/flex/application_form_presenter.rb
@@ -0,0 +1,42 @@
+module Flex
+  class ApplicationFormPresenter
+    attr_reader :application_form
+
+    def initialize(application_form, i18n_path)
+      @application_form = application_form
+      @i18n_path = i18n_path
+    end
+
+    def title
+      I18n.t("#{@i18n_path}.title")
+    end
+
+    def back_link_text
+      I18n.t("#{@i18n_path}.back")
+    end
+
+    def index_path
+      Rails.application.routes.url_helpers.send("#{@application_form.class.name.underscore.pluralize}_path")
+    end
+
+    def next_step
+      I18n.t("#{@i18n_path}.next_step.status.#{@application_form.status}")
+    end
+
+    def more_info_needed_heading
+      I18n.t("#{@i18n_path}.more_info_needed_heading")
+    end
+
+    def more_info_needed_item
+      I18n.t("#{@i18n_path}.more_info_needed_item")
+    end
+
+    def submitted_on_text
+      I18n.t("#{@i18n_path}.submitted_on")
+    end
+
+    def created_at
+      @application_form.created_at
+    end
+  end
+end
