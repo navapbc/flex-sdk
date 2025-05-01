54a36dc2a0b50b7fb4abeed41f03c8d3f2bd01cb
Loren Yu <loren@navapbc.com>
2025-05-01 16:19:34 -0700

    Syntax

--- template/{{app_name}}/engines/flex/spec/dummy/spec/views/passport_application_forms/index.html.erb_spec.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/spec/views/passport_application_forms/index.html.erb_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe "passport_application_forms/index.html.erb", type: :view do
   let(:passport_application_forms) do
-    p = PassportApplicationForm.new
-    p.first_name = "John"
-    p.last_name = "Doe"
-    p.date_of_birth = Date.new(1990, 1, 1)
-    p.created_at = Time.current
-    [ p ]
+    [
+      PassportApplicationForm.new(
+        first_name: "John",
+        last_name: "Doe",
+        date_of_birth: Date.new(1990, 1, 1),
+        created_at: Time.current
+      )
+    ]
   end
 
   it "renders passport application forms" do
