a717b324506798c01c177dfaa665b66f6f7f9493
Loren Yu <loren@navapbc.com>
2025-05-01 15:17:37 -0700

    Shorten routes

--- template/{{app_name}}/engines/flex/spec/dummy/config/routes.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  get "passport_application_forms/index"
-  get "passport_application_forms/show"
-  get "passport_cases/show"
   mount Flex::Engine => "/flex"
 
   resources :passport_cases do
@@ -9,4 +6,6 @@ Rails.application.routes.draw do
       get :closed
     end
   end
+
+  resources :passport_application_forms, only: [:index, :show]
 end
