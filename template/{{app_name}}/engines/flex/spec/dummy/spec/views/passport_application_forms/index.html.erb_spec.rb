1e41e5b6ba7caf6d4a588ac8da2f7b317a7f02d8
Loren Yu <loren@navapbc.com>
2025-05-01 16:17:24 -0700

    Add a test

--- template/{{app_name}}/engines/flex/spec/dummy/spec/views/passport_application_forms/index.html.erb_spec.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/spec/views/passport_application_forms/index.html.erb_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "passport_application_forms/index.html.erb", type: :view do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:passport_application_forms) do
+    p = PassportApplicationForm.new
+    p.first_name = "John"
+    p.last_name = "Doe"
+    p.date_of_birth = Date.new(1990, 1, 1)
+    p.created_at = Time.current
+    [ p ]
+  end
+
+  it "renders passport application forms" do
+    assign(:passport_application_forms, passport_application_forms)
+    render
+    expect(rendered).to match(/passport applications/i)
+    expect(rendered).to match(/new application/i)
+  end
 end
