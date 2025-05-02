53ac40f0664b2c24ad3d9627b1bee6e792e8168c
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-02 14:37:32 -0500

    Add flex tasks table migration

---
+++ template/{{app_name}}/engines/flex/spec/dummy/db/migrate/20250430160812_create_flex_tasks.rb
@@ -0,0 +1,12 @@
+class CreateFlexTasks < ActiveRecord::Migration[8.0]
+  def change
+    create_table :flex_tasks do |t|
+    t.string :type, index: true
+    t.text :description
+    t.integer :assignee_id, index: true # not linked to anything yet but will be later
+    t.integer :status, index: true, default: 0
+
+    t.timestamps
+    end
+  end
+end
