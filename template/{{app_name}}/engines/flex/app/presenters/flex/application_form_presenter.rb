d00efc0addf79e54d4e2c687d3f27f6f543c474b
Loren Yu <loren@navapbc.com>
2025-05-01 22:10:09 -0700

    Change presenter pattern to produce a hash

---
+++ template/{{app_name}}/engines/flex/app/presenters/flex/application_form_presenter.rb
@@ -0,0 +1,29 @@
+module Flex
+  class ApplicationFormPresenter
+    def initialize(application_form)
+      @application_form = application_form
+    end
+
+    def index
+      {
+        created_at: created_at,
+        object: object,
+        status: status
+      }
+    end
+
+    private
+
+    def created_at
+      @application_form.created_at.strftime("%B %d, %Y at %I:%M %p")
+    end
+
+    def object
+      @application_form
+    end
+
+    def status
+      I18n.t("flex.application_forms.status.#{@application_form.status}")
+    end
+  end
+end
--- template/{{app_name}}/engines/flex/app/presenters/flex/application_forms_presenter.rb
+++ template/{{app_name}}/engines/flex/app/presenters/flex/application_forms_presenter.rb
@@ -2,29 +2,37 @@ module Flex
   class ApplicationFormsPresenter
     attr_reader :application_forms
 
-    def initialize(application_forms, i18n_path)
+    def initialize(application_forms, controller_path)
       @application_forms = application_forms
-      @i18n_path = i18n_path
+      @controller_path = controller_path.gsub('/', '.')
     end
 
+    def index
+      {
+        title: title,
+        intro: intro,
+        new_button_text: new_button_text,
+        in_progress_applications_heading: in_progress_applications_heading,
+        application_forms: application_forms.map { |application_form| Flex::ApplicationFormPresenter.new(application_form).index }
+      }
+    end
+
+    private
+
     def title
-      I18n.t("#{@i18n_path}.title")
+      I18n.t("#{@controller_path}.index.title")
     end
 
     def intro
-      I18n.t("#{@i18n_path}.intro")
+      I18n.t("#{@controller_path}.index.intro")
     end
 
     def new_button_text
-      I18n.t("#{@i18n_path}.new_button")
+      I18n.t("#{@controller_path}.index.new_button")
     end
 
     def in_progress_applications_heading
-      I18n.t("#{@i18n_path}.in_progress_applications.heading")
-    end
-
-    def status_for(application_form)
-      I18n.t("flex.application_forms.status.#{application_form.status}")
+      I18n.t("#{@controller_path}.index.in_progress_applications.heading")
     end
   end
 end
