04dced5fba456a86b83db0307ec9e1d4c399df44
Loren Yu <loren@navapbc.com>
2025-05-01 18:42:27 -0700

    Add tests

--- template/{{app_name}}/engines/flex/spec/dummy/spec/requests/passport_application_forms_spec.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/spec/requests/passport_application_forms_spec.rb
@@ -1,16 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe "PassportApplicationForms", type: :request do
-  describe "GET /index" do
+  describe "GET /passport_application_forms" do
     it "returns http success" do
-      get "/passport_application_forms/index"
+      get "/passport_application_forms"
       expect(response).to have_http_status(:success)
     end
   end
 
-  describe "GET /show" do
+  describe "GET /passport_application_forms/:id" do
+    let(:passport_application_form) { PassportApplicationForm.create!(first_name: "John", last_name: "Doe", date_of_birth: Date.new(1990, 1, 1)) }
+
     it "returns http success" do
-      get "/passport_application_forms/show"
+      get "/passport_application_forms/#{passport_application_form.id}"
       expect(response).to have_http_status(:success)
     end
   end
