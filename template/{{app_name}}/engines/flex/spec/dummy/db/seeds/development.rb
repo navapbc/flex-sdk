0ddd01ebd64337cabc932bc2760814b3a3e65711
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-06 15:52:17 -0500

    Fix seeding date logic for tasks in past

--- template/{{app_name}}/engines/flex/spec/dummy/db/seeds/development.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/db/seeds/development.rb
@@ -4,10 +4,11 @@
   )
 end
 
+fifteen_days_ago = Date.current - 15.days
 30.times do |index|
   Flex::Task.create!(
     description: "Task description for #{index}",
-    due_on: Date.today + (index - (index / 2)).days,
+    due_on: fifteen_days_ago + index.days,
     case_id: PassportCase.pluck(:id).sample,
     assignee_id: nil # Will change this after adding user model in the future
   )
