55bdfed564af8b8e7fb0447721e6fd11d1a07a43
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-06 09:14:40 -0500

    Prevent updating assignee id or case id directly

--- template/{{app_name}}/engines/flex/app/models/flex/task.rb
+++ template/{{app_name}}/engines/flex/app/models/flex/task.rb
@@ -1,8 +1,12 @@
 module Flex
   class Task < ApplicationRecord
-    attribute :assignee_id, :string
-    attribute :case_id, :string
     attribute :description, :text
+
+    attribute :assignee_id, :string
+    protected attr_writer :assignee_id
+
+    attribute :case_id, :string
+    protected attr_writer :case_id
     
     attribute :status, :integer, default: 0
     protected attr_writer :status
@@ -11,17 +15,17 @@ module Flex
     validates :case_id, presence: true
 
     def set_case(case_id)
-      self.case_id = case_id
+      self[:case_id] = case_id
       save!
     end
 
     def assign(user_id)
-      self.assignee_id = user_id
+      self[:assignee_id] = user_id
       save!
     end
 
     def unassign
-      self.assignee_id = nil
+      self[:assignee_id] = nil
       save!
     end
 
--- template/{{app_name}}/engines/flex/spec/models/flex/task_spec.rb
+++ template/{{app_name}}/engines/flex/spec/models/flex/task_spec.rb
@@ -2,11 +2,30 @@ require 'rails_helper'
 
 RSpec.describe Flex::Task, type: :model do
   let(:kase) { TestCase.create! }
-  let(:task) { described_class.new(case_id: kase.id) }
+  let(:task) { described_class.new }
 
-  describe 'status attribute' do
-    it 'does not allow status to be modified directly' do
-      expect { task.status = :completed }.to raise_error(NoMethodError)
+  before do
+    task.set_case(kase.id)
+    task.description = 'Test task description'
+  end
+
+  context 'readonly attributes' do
+    describe 'status attribute' do
+      it 'cannot be modified directly' do
+        expect { task.status = :completed }.to raise_error(NoMethodError)
+      end
+    end
+
+    describe 'case_id attribute' do
+      it 'cannot be modified directly' do
+        expect { task.case_id = rand(1..1000).to_s }.to raise_error(NoMethodError)
+      end
+    end
+
+    describe 'assignee_id attribute' do
+      it 'cannot be modified directly' do
+        expect { task.assignee_id = rand(1..1000).to_s }.to raise_error(NoMethodError)
+      end
     end
   end
 
@@ -48,7 +67,7 @@ RSpec.describe Flex::Task, type: :model do
 
   describe 'validations' do
     it 'validates presence of case_id on create' do
-      expect { described_class.create!(case_id: nil) }.to raise_error(ActiveRecord::RecordInvalid, /Validation failed: Case can't be blank/)
+      expect { task.set_case(nil) }.to raise_error(ActiveRecord::RecordInvalid, /Validation failed: Case can't be blank/)
     end
   
     it 'validates presence of case_id on update' do
