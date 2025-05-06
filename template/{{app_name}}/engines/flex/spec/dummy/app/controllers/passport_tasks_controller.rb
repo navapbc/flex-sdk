820a7291f6b631138f562bbf0a03a455a2a26860
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-06 18:07:42 -0500

    Cleaned up the passport tasks controller

--- template/{{app_name}}/engines/flex/spec/dummy/app/controllers/passport_tasks_controller.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/app/controllers/passport_tasks_controller.rb
@@ -1,13 +1,6 @@
 class PassportTasksController < ApplicationController
   def index
-    if index_filter_params[:filter_date].present?
-      @tasks = filter_tasks_by_date(@tasks || Flex::Task, index_filter_params[:filter_date])
-    end
-    if index_filter_params[:filter_type].present?
-      @tasks = filter_tasks_by_type(@tasks || Flex::Task, index_filter_params[:filter_type])
-    end
-
-    (@tasks ||= Flex::Task.all).order_by_due_on_desc
+    filter_tasks
   end
 
   def new
@@ -30,30 +23,48 @@ class PassportTasksController < ApplicationController
     controller_path.classify.constantize
   end
 
+  
   private
-
   def index_filter_params
-    params.permit(:filter_date, :filter_type)
+    params.permit(:filter_date, :filter_type, :filter_status)
   end
 
-  def filter_tasks_by_date(tasks, filter_by)
+  def tasks
+    @tasks ||= Flex::Task.all
+  end
+
+  def filter_tasks
+    if index_filter_params[:filter_date].present?
+      @tasks = filter_tasks_by_date(index_filter_params[:filter_date])
+    end
+    if index_filter_params[:filter_type].present?
+      @tasks = filter_tasks_by_type(index_filter_params[:filter_type])
+    end
+    if index_filter_params[:filter_status].present?
+      @tasks = tasks.where_completed if index_filter_params[:filter_status] == "completed"
+    end
+
+    @tasks = tasks.order_by_due_on_desc
+  end
+
+  def filter_tasks_by_date(filter_by)
     case filter_by
     when "today"
-        tasks.filter_by_due_on(Date.today)
+        tasks.where_due_on(Date.today)
     when "overdue"
-        tasks.filter_by_due_on_before(Date.today)
+        tasks.where_due_on_before(Date.today)
     when "tomorrow"
-        tasks.filter_by_due_on(Date.tomorrow)
+        tasks.where_due_on(Date.tomorrow)
     when "this_week"
-        tasks.filter_by_due_on_range(Date.today.beginning_of_week, Date.today.end_of_week)
+        tasks.where_due_on_between(Date.today.beginning_of_week, Date.today.end_of_week)
     else
         tasks.all
     end
   end
 
-  def filter_tasks_by_type(tasks, filter_by)
-    Tasks.distinct_task_types.include?(filter_by) \
-      ? tasks.filter_by_type(filter_by)
+  def filter_tasks_by_type(filter_by)
+    tasks.select_distinct_task_types.include?(filter_by) \
+      ? tasks.where_type(filter_by)
       : tasks.all
   end
 end
