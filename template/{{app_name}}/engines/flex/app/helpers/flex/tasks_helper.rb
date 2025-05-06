c532a9a73de065f9aa5f920cd1c75c0c0e359e19
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-06 18:57:00 -0500

    Moved helper logic to flex engine
    Since the view is defined in Flex engine, it made more sense for the
    helper logic to also be in the Flex engine.

---
+++ template/{{app_name}}/engines/flex/app/helpers/flex/tasks_helper.rb
@@ -0,0 +1,34 @@
+module Flex
+  module TasksHelper
+    def mm_dd_yyyy(date)
+      date.strftime("%m/%d/%Y")
+    end
+
+    def time_since_epoch(date)
+      date.to_time.to_i
+    end
+
+    def distinct_task_type_options
+      task_types = Flex::Task.select_distinct_task_types.map do |type|
+        [ type.underscore.humanize, type ]
+      end
+
+      task_types.unshift([ "All tasks", "all" ])
+    end
+
+    def humanize_task_type(type)
+      type.underscore.humanize
+    end
+
+    def hidden_params_field(name)
+      hidden_field_tag(name, params[name]) if params[name].present?
+    end
+
+    def tabs_configuration(model_class)
+      [
+          { name: "Assigned", path: polymorphic_path(model_class, filter_status: nil, filter_date: params[:filter_date], filter_type: params[:filter_type]), active: params[:filter_status] != 'completed' },
+          { name: "Completed", path: polymorphic_path(model_class, filter_status: 'completed', filter_date: params[:filter_date], filter_type: params[:filter_type]), active: params[:filter_status] == 'completed' }
+      ]
+    end
+  end
+end
