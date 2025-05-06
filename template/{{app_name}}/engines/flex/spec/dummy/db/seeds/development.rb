07ba9ad890efc2071e8fc07bd76c46e89d530240
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-06 15:35:27 -0500

    Added basic seeding functionality to the dummy app

---
+++ template/{{app_name}}/engines/flex/spec/dummy/db/seeds/development.rb
@@ -0,0 +1,14 @@
+5.times do |index|
+  PassportCase.create!(
+    passport_id: SecureRandom.uuid,
+  )
+end
+
+30.times do |index|
+  Flex::Task.create!(
+    description: "Task description for #{index}",
+    due_on: Date.today + (index - (index / 2)).days,
+    case_id: PassportCase.pluck(:id).sample,
+    assignee_id: nil
+  )
+end
