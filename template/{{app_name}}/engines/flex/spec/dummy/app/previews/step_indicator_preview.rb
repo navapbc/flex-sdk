af55f98bc62dc662beacfe4e40b2fbb596d08641
Loren Yu <loren@navapbc.com>
2025-05-07 10:02:17 -0700

    Add step indicator preview

---
+++ template/{{app_name}}/engines/flex/spec/dummy/app/previews/step_indicator_preview.rb
@@ -0,0 +1,24 @@
+class StepIndicatorPreview < Lookbook::Preview
+  layout "component_preview"
+
+  def default
+    render template: "flex/shared/_step_indicator", locals: {
+      steps: [:in_progress, :submitted, :decision_made],
+      current_step: :in_progress
+    }
+  end
+
+  def submitted_state
+    render template: "flex/shared/_step_indicator", locals: {
+      steps: [:in_progress, :submitted, :decision_made],
+      current_step: :submitted
+    }
+  end
+
+  def decision_made_state
+    render template: "flex/shared/_step_indicator", locals: {
+      steps: [:in_progress, :submitted, :decision_made],
+      current_step: :decision_made
+    }
+  end
+end
