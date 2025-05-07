e1238b7a33cac6ff9c422572dddd452a594e68c7
Loren Yu <loren@navapbc.com>
2025-05-07 14:23:48 -0700

    Move attributes concern to lib

--- template/{{app_name}}/engines/flex/lib/flex/engine.rb
+++ template/{{app_name}}/engines/flex/lib/flex/engine.rb
@@ -1,5 +1,8 @@
 module Flex
   class Engine < ::Rails::Engine
     isolate_namespace Flex
+
+    config.autoload_paths += %W[#{config.root}/lib]
+    config.eager_load_paths += %W[#{config.root}/lib]
   end
 end
