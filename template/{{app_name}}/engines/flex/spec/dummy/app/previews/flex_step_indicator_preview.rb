c0c3eb84f75401e0e967d1290e811f1f51439a8a
Loren Yu <loren@navapbc.com>
2025-05-07 10:52:57 -0700

    Lint and test

--- template/{{app_name}}/engines/flex/spec/dummy/app/previews/flex_step_indicator_preview.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/app/previews/flex_step_indicator_preview.rb
@@ -5,21 +5,21 @@ class FlexStepIndicatorPreview < Lookbook::Preview
 
   def in_progress
     render template: "flex/shared/_step_indicator", locals: {
-      steps: [:in_progress, :submitted, :decision_made],
+      steps: [ :in_progress, :submitted, :decision_made ],
       current_step: :in_progress
     }
   end
 
   def submitted
     render template: "flex/shared/_step_indicator", locals: {
-      steps: [:in_progress, :submitted, :decision_made],
+      steps: [ :in_progress, :submitted, :decision_made ],
       current_step: :submitted
     }
   end
 
   def decision_made
     render template: "flex/shared/_step_indicator", locals: {
-      steps: [:in_progress, :submitted, :decision_made],
+      steps: [ :in_progress, :submitted, :decision_made ],
       current_step: :decision_made
     }
   end
--- template/{{app_name}}/engines/flex/spec/dummy/config/routes.rb
+++ template/{{app_name}}/engines/flex/spec/dummy/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   mount Flex::Engine => "/flex"
 
-  mount Lookbook::Engine, at: "/lookbook"
+  mount Lookbook::Engine, at: "/lookbook" if Rails.env.development?
 
   resources :passport_cases do
     collection do
