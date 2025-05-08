035838e647b113e216faa1dcf42a12f166fa66fd
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-08 13:48:58 -0500

    Add migration for users table

---
+++ template/{{app_name}}/engines/flex/spec/dummy/db/migrate/20250508184707_add_users.rb
@@ -0,0 +1,11 @@
+class AddUsers < ActiveRecord::Migration[8.0]
+  def change
+    create_table :users do |t|
+      t.string :name
+
+      t.timestamps
+    end
+
+    add_foreign_key :flex_tasks, :users, column: :assignee_id, primary_key: :id, on_delete: :restrict
+  end
+end
