ccee667865fa15540575b6fa4557dab9b68c8c62
Loren Yu <loren@navapbc.com>
2025-05-07 18:33:33 -0700

    Try fixing tests

--- template/{{app_name}}/engines/flex/lib/flex/engine.rb
+++ template/{{app_name}}/engines/flex/lib/flex/engine.rb
@@ -2,7 +2,9 @@ module Flex
   class Engine < ::Rails::Engine
     isolate_namespace Flex
 
-    config.autoload_paths += %W[#{config.root}/lib]
-    config.eager_load_paths += %W[#{config.root}/lib]
+    initializer "flex.autoload", before: :set_autoload_paths do |app|
+      config.autoload_paths += %W[#{config.root}/lib]
+      config.eager_load_paths += %W[#{config.root}/lib]
+    end
   end
 end
