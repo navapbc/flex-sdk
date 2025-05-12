e4a17234006de752af5e05fa0f81affdf111f9e6
Loren Yu <loren@navapbc.com>
2025-05-12 15:34:33 -0700

    Fix migration

---
+++ spec/dummy/db/migrate/20250512223206_add_period_to_test_records.rb
@@ -0,0 +1,6 @@
+class AddPeriodToTestRecords < ActiveRecord::Migration[8.0]
+  def change
+    add_column :test_records, :period_start, :date
+    add_column :test_records, :period_end, :date
+  end
+end
