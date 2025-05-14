579a3bf19c6e48e0ffbe417f15b3ff7baf4bdea8
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-14 12:37:40 -0500

    Updated seeding after schema changes

--- spec/dummy/db/seeds/development.rb
+++ spec/dummy/db/seeds/development.rb
@@ -1,8 +1,7 @@
-10.times do |index|
+users = 10.times.collect do |index|
   User.create!(
     first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    id: SecureRandom.uuid
+    last_name: Faker::Name.last_name
   )
 end
 
@@ -13,16 +12,17 @@ end
     date_of_birth: Faker::Date.birthday(min_age: 0, max_age: 130),
   )
 end
+passport_cases = PassportCase.all
 
 ten_days_ago = Date.current - 10.days
 20.times do |index|
   task = PassportVerifyInfoTask.create!(
     description: "Task description for #{index}",
     due_on: ten_days_ago + index.days,
-    case_id: PassportCase.pluck(:id).sample
+    case_id: passport_cases.sample.id
   )
 
-  task.assign(User.pluck(:id).sample)
+  task.assign(users.sample.id)
   task.mark_completed if rand(0..2) == 0
 end
 
@@ -30,9 +30,9 @@ end
   task = PassportPhotoTask.create!(
     description: "Task description for #{index}",
     due_on: ten_days_ago + index.days,
-    case_id: PassportCase.pluck(:id).sample
+    case_id: passport_cases.sample.id
   )
 
-  task.assign(User.pluck(:id).sample)
+  task.assign(users.sample.id)
   task.mark_completed if rand(0..5) == 0
 end
