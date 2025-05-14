0921eb910da988e6d4b0f1a6b1bbb27ac2706b0c
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-14 14:49:02 -0500

    Remove passport task config initializer

--- spec/dummy/config/initializers/passport_task_configuration.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.config.to_prepare do
-  Rails.configuration.passport = {
-    task_type_map: {
-      PassportPhotoTask => 'Verify Photo',
-      PassportVerifyInfoTask => 'Verify Info'
-    }
-  }
-end
