1f874b6a33f75760e9c967d35aac139918f6bb41
Loren Yu <loren@navapbc.com>
2025-05-15 08:09:37 -0700

    Add initializer in engine.rb that calls BusinessProcess.start_listening_for_events

--- lib/flex/engine.rb
+++ lib/flex/engine.rb
@@ -11,5 +11,11 @@ module Flex
     initializer "flex.previews" do |app|
       config.lookbook.preview_paths << Flex::Engine.root.join("app", "previews") if config.respond_to?(:lookbook)
     end
+
+    initializer "flex.start_business_processes" do |app|
+      config.to_prepare do
+        Flex::BusinessProcess.start_listening_for_events
+      end
+    end
   end
 end
