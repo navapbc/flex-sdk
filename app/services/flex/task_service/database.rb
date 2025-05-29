a06824d40da168ce78867bf89c53242ee3911035
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-05-29 11:02:23 -0500

    Fix task naming to task_class

--- app/services/flex/task_service/database.rb
+++ app/services/flex/task_service/database.rb
@@ -4,7 +4,7 @@ module Flex
     # Implements the TaskHandlerService interface
     class Database < Base
       def create_task(task_class, kase)
-        raise ArgumentError, "`task` must be a Flex::Task or a subclass of Flex::Task" unless task_class.present? && task <= (Flex::Task)
+        raise ArgumentError, "`task_class` must be a Flex::Task or a subclass of Flex::Task" unless task_class.present? && task <= (Flex::Task)
         raise ArgumentError, "`kase` must be a subclass of Flex::Case" unless kase.present? && kase.is_a?(Flex::Case)
         task = task_class.from_case(kase)
         task.save!
