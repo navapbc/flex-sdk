83c1db70b8caa7e2311190156cbe18d33d697a99
Loren Yu <loren@navapbc.com>
2025-06-02 14:23:24 -0700

    Fix preview

--- app/previews/flex/address_preview.rb
+++ app/previews/flex/address_preview.rb
@@ -23,18 +23,6 @@ module Flex
       render template: "flex/previews/_address", locals: { model: model }
     end
 
-    def invalid
-      model = new_model
-      # Set invalid state by setting address fields but leaving required fields blank
-      model.address_street_line_1 = "123 Main St"
-      model.address_street_line_2 = "Apt 4B"
-      model.address_city = "" # Invalid: required field is blank
-      model.address_state = "" # Invalid: required field is blank
-      model.address_zip_code = "invalid" # Invalid: doesn't match pattern
-      model.valid?
-      render template: "flex/previews/_address", locals: { model: model }
-    end
-
     def custom_legend
       model = new_model
       render template: "flex/previews/_address", locals: { model: model, legend: "Custom Address Legend" }
@@ -43,22 +31,7 @@ module Flex
     private
 
     def new_model
-      @model = Class.new do
-        include ActiveModel::Model
-        include ActiveModel::Attributes
-        include ActiveModel::Validations
-        include Flex::Attributes::AddressAttribute
-
-        address_attribute :address
-
-        validates :address_city, presence: true
-        validates :address_state, presence: true
-        validates :address_zip_code, format: { with: /\A[0-9]{5}(-[0-9]{4})?\z/, message: "must be a valid ZIP code" }, allow_blank: true
-
-        def self.model_name
-          ActiveModel::Name.new(self, nil, "TestModel")
-        end
-      end.new
+      TestRecord.new
     end
   end
 end
