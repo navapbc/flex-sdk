5dbdf86d47f5d39b442ef76c220f9e0119f4e5cc
Loren Yu <loren@navapbc.com>
2025-06-04 14:45:04 -0700

    Simplify

--- spec/dummy/app/previews/business_process_preview.rb
+++ spec/dummy/app/previews/business_process_preview.rb
@@ -1,6 +1,4 @@
 class BusinessProcessPreview < Lookbook::Preview
-  layout "mermaid_diagram"
-
   def passport_business_process
     render template: "flex/previews/_business_process", locals: { business_process: PassportBusinessProcess }
   end
