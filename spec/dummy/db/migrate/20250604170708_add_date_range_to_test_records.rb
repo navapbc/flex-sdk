dc955a0a40c0480c84ff08f5bdce752bc61b69fe
Loren Yu <loren@navapbc.com>
2025-06-04 18:05:53 -0700

    Fix migration

--- spec/dummy/db/migrate/20250604170708_add_date_range_to_test_records.rb
+++ spec/dummy/db/migrate/20250604170708_add_date_range_to_test_records.rb
@@ -1,6 +1,6 @@
 class AddDateRangeToTestRecords < ActiveRecord::Migration[8.0]
   def change
-    add_column :test_records, :date_range_start, :date
-    add_column :test_records, :date_range_end, :date
+    add_column :test_records, :period_start, :date
+    add_column :test_records, :period_end, :date
   end
 end
