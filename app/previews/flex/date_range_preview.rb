6ac9058b2fc70de77572cca574439c937743e811
Loren Yu <loren@navapbc.com>
2025-06-05 15:08:58 -0700

    Adjust previews

--- app/previews/flex/date_range_preview.rb
+++ app/previews/flex/date_range_preview.rb
@@ -23,15 +23,15 @@ module Flex
 
     def invalid_start_date
       record = TestRecord.new
-      record.period_start = "2023-02-45"
-      record.period_end = Date.new(2023, 12, 31)
+      record.period_start = "02/45/2023"
+      record.period_end = "12/31/2023"
       record.valid?
       render template: "flex/previews/_date_range", locals: { model: record }
     end
 
     def invalid_end_date
       record = TestRecord.new
-      record.period_start = Date.new(2023, 1, 1)
+      record.period_start = "01/02/2023"
       record.period_end = "not-a-date"
       record.valid?
       render template: "flex/previews/_date_range", locals: { model: record }
