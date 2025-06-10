0b12e19451fb1a90698a685540c28c66e96d2da1
Loren Yu <loren@navapbc.com>
2025-06-10 13:11:38 -0700

    Change default to date range

--- lib/generators/flex/income_records_migration/income_records_migration_generator.rb
+++ lib/generators/flex/income_records_migration/income_records_migration_generator.rb
@@ -10,12 +10,13 @@ module Flex
     #   rails generate flex:income_records_migration CreateIncomeRecords date_range
     #
     class IncomeRecordsMigrationGenerator < Rails::Generators::NamedBase
-      argument :period_type, type: :string, default: "year_quarter", banner: "period_type"
+      argument :period_type, type: :string, default: "date_range", banner: "period_type"
 
       def create_migration_file
         columns = [ "person_id:string", "amount:integer" ]
 
         raise ArgumentError, "Unsupported period type: #{period_type}. Use 'year_quarter' or 'date_range'" if ![ "year_quarter", "date_range" ].include?(period_type)
+
         columns << "period:#{period_type}"
 
         generate("flex:migration", name, *columns)
