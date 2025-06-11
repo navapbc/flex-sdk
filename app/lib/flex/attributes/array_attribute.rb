d742e5f5216c1121100d498ba93e6952bec5d235
Loren Yu <loren@navapbc.com>
2025-06-11 10:35:52 -0700

    Create ArrayType

--- app/lib/flex/attributes/array_attribute.rb
+++ app/lib/flex/attributes/array_attribute.rb
@@ -11,6 +11,21 @@ module Flex
     module ArrayAttribute
       extend ActiveSupport::Concern
 
+      class ArrayType < ActiveModel::Type::Value
+        def cast(value)
+          Array(value)
+        end
+
+        def serialize(value)
+          value.to_json
+        end
+
+        def deserialize(value)
+          return [] if value.nil?
+          JSON.parse(value)
+        end
+      end
+
       class_methods do
         # Defines an attribute representing an array of value objects.
         #
@@ -18,7 +33,7 @@ module Flex
         # @param [Hash] options Options for the attribute
         # @return [void]
         def array_attribute(name, options = {})
-          attribute name, :json, default: []
+          attribute name, ArrayType.new, default: []
           validate :"validate_#{name}"
 
           # Create a validation method that validates each of the value objects
