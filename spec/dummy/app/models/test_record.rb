054c7b08ddbf718775f02438efe9f3d642cf908b
Loren Yu <loren@navapbc.com>
2025-06-11 10:04:03 -0700

    Define array attributes in TestRecord

--- spec/dummy/app/models/test_record.rb
+++ spec/dummy/app/models/test_record.rb
@@ -8,4 +8,9 @@ class TestRecord < ApplicationRecord
   flex_attribute :period, :date_range
   flex_attribute :tax_id, :tax_id
   flex_attribute :reporting_period, :year_quarter
+
+  # Array types
+  flex_attribute :addresses, :array
+  flex_attribute :names, :array
+  flex_attribute :reporting_periods, :array
 end
