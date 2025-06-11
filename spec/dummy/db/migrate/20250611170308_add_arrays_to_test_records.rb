afffaf1d37a19bb43cf454d3c636389cbdba7642
Loren Yu <loren@navapbc.com>
2025-06-11 10:03:31 -0700

    Use generator to add arrays to test_records
    Ran `rails generate flex:migration AddArraysToTestRecords addresses:array names:array reporting_periods:array`

---
+++ spec/dummy/db/migrate/20250611170308_add_arrays_to_test_records.rb
@@ -0,0 +1,7 @@
+class AddArraysToTestRecords < ActiveRecord::Migration[8.0]
+  def change
+    add_column :test_records, :addresses, :jsonb
+    add_column :test_records, :names, :jsonb
+    add_column :test_records, :reporting_periods, :jsonb
+  end
+end
