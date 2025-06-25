9d5109295ebc231e3cdd034239c4322e04123acf
Loren Yu <loren@navapbc.com>
2025-06-24 22:21:14 -0400

    Run rails flex:install:migrations on dummy app

---
+++ spec/dummy/db/migrate/20250625021958_create_flex_business_processes.flex.rb
@@ -0,0 +1,15 @@
+# This migration comes from flex (originally 20250624223240)
+class CreateFlexBusinessProcesses < ActiveRecord::Migration[8.0]
+  def change
+    create_table :flex_business_processes do |t|
+      t.string :type, index: true
+      t.uuid :case_id, index: true
+      t.string :case_type
+      t.string :current_step
+
+      t.timestamps
+    end
+
+    # TODO migrate current_step from cases tables in dummy apps
+  end
+end
