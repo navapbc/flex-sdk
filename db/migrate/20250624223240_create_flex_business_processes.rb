5194f9051f7be68bfb390c704f091d5e928203ce
Loren Yu <loren@navapbc.com>
2025-06-26 22:48:27 -0400

    Add commented out example migration to migrate current_step from cases to business processes table

--- db/migrate/20250624223240_create_flex_business_processes.rb
+++ db/migrate/20250624223240_create_flex_business_processes.rb
@@ -1,6 +1,7 @@
+# This migration comes from flex (originally 20250624223240)
 class CreateFlexBusinessProcesses < ActiveRecord::Migration[8.0]
   def change
-    create_table :flex_business_processes, id: :uuid do |t|
+    create_table :flex_business_processes do |t|
       t.string :type, index: true
       t.uuid :case_id, index: true
       t.string :case_type
@@ -9,6 +10,21 @@ class CreateFlexBusinessProcesses < ActiveRecord::Migration[8.0]
       t.timestamps
     end
 
-    # TODO migrate current_step from cases tables in dummy apps
+    # Uncomment the following lines to bulk migrate existing cases to
+    # flex_business_processes.
+    # You'll want to do this for each case type you have in your application.
+    # Replace "MyBusinessProcess" and "MyCase" with your actual business process
+    # and case model names.
+    # 
+    # values = MyCase.open.map do |kase|
+    #   "('MyBusinessProcess', '#{kase.id}', 'MyCase', '#{kase.business_process_current_step}', NOW(), NOW())"
+    # end
+    #
+    # if values.any?
+    #   execute <<-SQL
+    #     INSERT INTO flex_business_processes (type, case_id, case_type, current_step, created_at, updated_at)
+    #     VALUES #{values.join(",")}
+    #   SQL
+    # end
   end
 end
