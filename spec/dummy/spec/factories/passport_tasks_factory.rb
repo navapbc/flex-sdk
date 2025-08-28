aa341b8c7df3f80bba28769507a481ff5fdc6d01
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-08-28 10:57:58 -0500

    Add `with_` to trait names

--- spec/dummy/spec/factories/passport_tasks_factory.rb
+++ spec/dummy/spec/factories/passport_tasks_factory.rb
@@ -2,10 +2,10 @@ FactoryBot.define do
   factory :passport_task do
     association :case, factory: :passport_case
 
-    trait(:description) do
+    trait :with_description do
       description { Faker::Lorem.sentence }
     end
-    trait(:due_on) do
+    trait :with_due_on do
       due_on { 1.week.from_now }
     end
   end
