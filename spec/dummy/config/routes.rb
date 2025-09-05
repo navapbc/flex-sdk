67260cbba7b3e4074fffb55992d624f433b4d035
Loren Yu <loren@navapbc.com>
2025-09-05 15:01:24 -0700

    Move routes under /staff

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -3,25 +3,28 @@ Rails.application.routes.draw do
 
   mount Lookbook::Engine, at: "/lookbook"
 
-  resources :passport_cases do
-    resources :tasks
-
-    collection do
-      get :closed
-    end
-
-    member do
-      get :application_details
-      get :documents
-      get :history
-      get :notes
-    end
-  end
-
   resources :passport_application_forms, only: [ :index, :new, :show, :create ]
-  resources :tasks, only: [ :index, :show, :update ] do
-    collection do
-      post :pick_up_next_task
+
+  scope path: "/staff" do
+    resources :passport_cases do
+      resources :tasks, shallow: true
+  
+      collection do
+        get :closed
+      end
+  
+      member do
+        get :application_details
+        get :documents
+        get :history
+        get :notes
+      end
+    end
+
+    resources :tasks, only: [ :index, :show, :update ] do
+      collection do
+        post :pick_up_next_task
+      end
     end
   end
 
