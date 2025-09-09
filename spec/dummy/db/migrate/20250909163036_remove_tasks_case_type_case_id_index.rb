3e2bc8777d1f1ab2d9d34d8621a3a844ae84b546
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2025-09-09 11:35:04 -0500

    Remove combined index in favor of individual

---
+++ spec/dummy/db/migrate/20250909163036_remove_tasks_case_type_case_id_index.rb
@@ -0,0 +1,7 @@
+class RemoveTasksCaseTypeCaseIdIndex < ActiveRecord::Migration[8.0]
+  def change
+    remove_index :flex_tasks, [ :case_id, :case_type ]
+    add_index :flex_tasks, [ :case_id ]
+    add_index :flex_tasks, [ :case_type ]
+  end
+end
