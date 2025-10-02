0071db556ab3d4dd70236fdee639cbac1254eeaa
Loren Yu <loren@navapbc.com>
2025-10-02 15:53:28 -0700

    Call path_func

--- app/components/strata/cases/case_row_component.rb
+++ app/components/strata/cases/case_row_component.rb
@@ -2,8 +2,9 @@
 
 module Strata
   class Cases::CaseRowComponent < ViewComponent::Base
-    def initialize(kase:)
+    def initialize(kase:, path_func: method(:polymorphic_path))
       @case = kase
+      @path_func = path_func
     end
   end
 end
