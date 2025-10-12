efdbee7a03bc4a4ae2a6296582b36da6d64da5b8
Loren Yu <loren@navapbc.com>
2025-10-11 19:14:10 -0700

    Add docs

--- app/controllers/strata/staff_controller.rb
+++ app/controllers/strata/staff_controller.rb
@@ -2,7 +2,26 @@
 
 module Strata
   # Base controller for all staff-related functionality.
-  # Sets the layout to the strata/staff layout
+  # Sets the layout to the strata/staff layout.
+  #
+  # To customize the header in a subclass of StaffController, override the following methods:
+  # - case_classes: Specify which case types to display as links in the header.
+  # - tasks_links: Customize or add additional links related to tasks.
+  # - header_links: Completely override this method to fully control the header links if needed.
+  #
+  # Example:
+  #   def case_classes
+  #     [ PassportCase, VisaCase ]
+  #   end
+  #
+  #   def tasks_links
+  #     super + [{ name: "My Custom Task", path: custom_task_path }]
+  #   end
+  #
+  #   def header_links
+  #     # Fully customize header links if default combining isn't sufficient
+  #     [ { name: "Home", path: home_path } ] + super
+  #   end
   class StaffController < ApplicationController
     layout "strata/staff"
 
