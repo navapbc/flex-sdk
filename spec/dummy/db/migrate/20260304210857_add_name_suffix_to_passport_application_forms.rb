573edea316b830a4e756d04d267e89ab782da2b6
Roger Lam <rogerlam@navapbc.com>
2026-03-04 13:24:28 -0800

    lint

--- spec/dummy/db/migrate/20260304210857_add_name_suffix_to_passport_application_forms.rb
+++ spec/dummy/db/migrate/20260304210857_add_name_suffix_to_passport_application_forms.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddNameSuffixToPassportApplicationForms < ActiveRecord::Migration[8.0]
   def change
     add_column :passport_application_forms, :name_suffix, :string
