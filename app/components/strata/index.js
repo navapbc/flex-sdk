89b9434790421db9cc1f11f60d244b7d2f478ff9
Roger Lam <rogerlam@navapbc.com>
2026-03-05 15:42:20 -0800

    Add comment about adding controllers to import function

--- app/components/strata/index.js
+++ app/components/strata/index.js
@@ -2,6 +2,8 @@ import ConditionalFieldComponentController from "./conditional_field_component_c
 
 export { ConditionalFieldComponentController }
 
-export function registerControllers(application) {
+// As we add more components with Stimulus, add the Controller to this function to make
+// importing easier
+export default function registerControllers(application) {
   application.register("strata--conditional-field", ConditionalFieldComponentController)
 }
