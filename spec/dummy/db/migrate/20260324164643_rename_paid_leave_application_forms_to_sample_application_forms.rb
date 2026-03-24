4acf195e4a75f8d352cc1c6c0c755a4b03007865
Roger Lam <rogerlam@navapbc.com>
2026-03-24 09:47:47 -0700

    Update tables with migration;

---
+++ spec/dummy/db/migrate/20260324164643_rename_paid_leave_application_forms_to_sample_application_forms.rb
@@ -0,0 +1,5 @@
+class RenamePaidLeaveApplicationFormsToSampleApplicationForms < ActiveRecord::Migration[8.0]
+  def change
+    rename_table :paid_leave_application_forms, :sample_application_forms
+  end
+end
