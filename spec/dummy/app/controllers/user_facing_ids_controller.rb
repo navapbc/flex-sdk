5ce2eb9c1b057c56fbae8c87e38c670bcd8cc37e
MichaelCrawfordNava <michaelcrawford@navapbc.com>
2026-05-06 15:33:35 -0500

    Use in-memory records rather than persisted records

--- spec/dummy/app/controllers/user_facing_ids_controller.rb
+++ spec/dummy/app/controllers/user_facing_ids_controller.rb
@@ -4,16 +4,12 @@ class UserFacingIdsController < ApplicationController
   SAMPLE_RECORD_COUNT = 8
 
   def index
-    create_sample_records if TestRecord.count < SAMPLE_RECORD_COUNT
-
-    @test_records = TestRecord
-      .order(:user_facing_id_sequence)
-      .limit(SAMPLE_RECORD_COUNT)
-  end
-
-  private
-
-  def create_sample_records
-    (SAMPLE_RECORD_COUNT - TestRecord.count).times { TestRecord.create! }
+    @test_records = SAMPLE_RECORD_COUNT.times.map do |index|
+      sequence = index + 1
+      TestRecord.new(
+        user_facing_id_sequence: sequence,
+        claim_user_facing_id_sequence: sequence
+      )
+    end
   end
 end
